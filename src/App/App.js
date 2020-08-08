5c573da82ab94c0c12024746b1f7f26bfb781c82
Billy Campbell <wcampbell91@gmail.com>
2020-08-08 11:49:43 -0500

    setup files for react app

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>INSIDE THIS THANG</h2>
+        <button className="btn btn-info"><i class="fas fa-music"></i>BUTTOON</button>
+      </div>
+    );
+  }
+}
+
+export default App;
