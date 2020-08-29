87092f38960b02bdeee5305a1cfee5c98330c672
Billy Campbell <wcampbell91@gmail.com>
2020-08-29 11:22:27 -0500

    added react strap navbar using hooks!

--- src/components/pages/MyNavbar/MyNavbar.js
+++ src/components/pages/MyNavbar/MyNavbar.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const MyNavbar = () => {
+const MyNavbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   const logMeOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   };
 
   return (
-    <div className="MyNavbar">
-      <h1>My Navbar</h1>
-      <button className="btn btn-danger" onClick={logMeOut}> Logout</button>
+    // <div className="MyNavbar">
+    //   <h1>My Navbar</h1>
+    //   <button className="btn btn-danger" onClick={logMeOut}> Logout</button>
+    // </div>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
     </div>
   );
 };
