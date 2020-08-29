a85e6ee4377c87446e0ed5ddad962f81972a91f1
Billy Campbell <wcampbell91@gmail.com>
2020-08-29 10:43:28 -0500

    basic routing setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
@@ -14,6 +20,19 @@ import SingleBirb from '../components/pages/SingleBirb/SingleBirb';
 import './App.scss';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -35,15 +54,32 @@ class App extends React.Component {
   }
 
   render() {
+    const { authed } = this.state;
     return (
       <div className="App">
-        <h1><span role="img" aria-label="bird emoji">🦉</span> Watcher!</h1>
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar />
+            <div className="container">
+              <Switch>
+                <PrivateRoute path="/home" component={Home} authed={authed} />
+                <PrivateRoute path="/new" component={NewBirb} authed={authed} />
+                <PrivateRoute path="/edit:birbId" component={EditBirb} authed={authed} />
+                <PrivateRoute path="/birbs:birbId" component={SingleBirb} authed={authed} />
+                <PublicRoute path="/auth" component={Auth} authed={authed} />
+                <Redirect from="*" to="/home" />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
+        {/* {<h1><span role="img" aria-label="bird emoji">🦉</span> Watcher!</h1>
         <MyNavbar />
         <Auth />
+
         <Home />
         <SingleBirb />
         <NewBirb />
-        <EditBirb />
+        <EditBirb />} */}
       </div>
     );
   }
